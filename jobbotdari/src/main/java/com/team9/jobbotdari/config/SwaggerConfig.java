082e32153061383486b84b0e038ae07bc2a150e4
Jihoony_.c <66457014+cjh-19@users.noreply.github.com>
2025-02-23 17:48:29 +0900

    [FEAT #30] config 서버 & eureka 서버 & gateway 서버 & 유저 및 기업 서버 yml 설정 (#31)
    * [FEAT #26] 기업 서버 swagger 미적용 수정
    
    * [FEAT #30] config 서버 생성 및 각 서비스 서버 yml, properties 수정
    
    * [FEAT #30] eureka 서버 생성
    
    * [FEAT #30] gateway 서버 생성

--- jobbotdari/src/main/java/com/team9/jobbotdari/config/SwaggerConfig.java
+++ jobbotdari/src/main/java/com/team9/jobbotdari/config/SwaggerConfig.java
@@ -1,2 +1,35 @@
-package com.team9.jobbotdari.config;public class SwaggerConfig {
+package com.team9.jobbotdari.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig {
+
+    public SwaggerConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("취업봇다리")
+                .description("취업봇다리 기업 서버 REST API")
+                .version("1.0.0");
+    }
+
 }
