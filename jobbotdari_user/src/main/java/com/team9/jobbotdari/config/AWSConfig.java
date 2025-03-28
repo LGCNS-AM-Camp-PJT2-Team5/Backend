3a204251080ca83916f80e4bf4837e531dce9ee0
coffeesigma <ljb22060@gmail.com>
2025-03-28 22:14:20 +0900

    change version aws, delete old files

--- jobbotdari_user/src/main/java/com/team9/jobbotdari/config/AWSConfig.java
+++ jobbotdari_user/src/main/java/com/team9/jobbotdari/config/AWSConfig.java
@@ -1,14 +1,14 @@
 package com.team9.jobbotdari.config;
 
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AWSConfig {
@@ -26,13 +26,13 @@ public class AWSConfig {
     private String bucketName;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public S3Client S3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
 
         AmazonS3ClientBuilder clientBuilder = AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials));
 
-        return (AmazonS3Client) clientBuilder.build();
+        return (S3Client) clientBuilder.build();
     }
 }
--- jobbotdari_user/src/main/java/com/team9/jobbotdari/service/ProfileService.java
+++ jobbotdari_user/src/main/java/com/team9/jobbotdari/service/ProfileService.java
@@ -11,6 +11,7 @@ import com.team9.jobbotdari.repository.UserRepository;
 import com.team9.jobbotdari.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
